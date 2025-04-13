12463e98389e310cdced2bc04698be9df440191b
Yash Shelar <118765043+YashShelar04@users.noreply.github.com>
2025-04-13 12:41:12 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,5 +2,5 @@ DB_CONFIG = {
     'host': 'ecom-db.c8n22iusy24s.us-east-1.rds.amazonaws.com',
     'user': 'admin',
     'password': 'YashShelar2004',
-    'database': 'ecom-db'
+    'database': 'my_database'
 }
